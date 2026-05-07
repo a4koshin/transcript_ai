efbfc4b9fd905cad8a6ea21d149a44d1fc4a9979
a4koshin <mankajr11@gmail.com>
2026-05-07 16:24:09 +0300

    Config is change on something

--- backend/app/core/config.py
+++ backend/app/core/config.py
@@ -1,12 +1,6 @@
-# UPLOAD_DIR = "uploads"
-# TRANSCRIPT_DIR = "transcripts"
-# MODEL_NAME = "medium"
-# ALLOWED_ORIGINS = ["http://localhost:3000"]
-
-
-
 UPLOAD_DIR = "uploads"
 TRANSCRIPT_DIR = "transcripts"
 MODEL_NAME = "facebook/mms-1b-all"
 TARGET_LANGUAGE = "som"
 ALLOWED_ORIGINS = ["http://localhost:3000"]
+

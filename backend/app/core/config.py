017514902e38325d8ae62030eb2cf71bae060e97
a4koshin <mankajr11@gmail.com>
2026-05-07 22:35:50 +0300

    Update app config

--- backend/app/core/config.py
+++ backend/app/core/config.py
@@ -1,6 +1,6 @@
 UPLOAD_DIR = "uploads"
 TRANSCRIPT_DIR = "transcripts"
+CHUNK_DIR = "chunks"
 MODEL_NAME = "facebook/mms-1b-all"
 TARGET_LANGUAGE = "som"
 ALLOWED_ORIGINS = ["http://localhost:3000"]
-
